8cf7d097d31a9b8282a31001df81a1a2181fe410
benjaminadams <armastevs@gmail.com>
2014-05-29 04:36:06 +0000

    cleaned up some stuff

--- public/wp-redditjs.php
+++ public/wp-redditjs.php
@@ -299,19 +299,11 @@ class WP_Redditjs {
 		$heightpx= $height . 'px';
 		//$embedURL = "http://localhost:8002/embed?url=$currentURL&as=4&submitPostImg=$submitPostImg&postSortOrder=$postSortOrder";
 		//echo "<div class='redditjs_iframe_wrapper'><iframe id='redditjs_post' src='$embedURL' ></iframe></div>";
-		echo "<script id='rjs_post' src='//localhost:8002/post.js' data-width='$width' data-height='$height' data-postSortOrder='$postSortOrder'  ></script>";
- 
-
-		?>
-	<script>
-	// window.wp_redditjs = {}
-	// //passing users desired iframe height and width to the global object
-	// window.wp_redditjs.height= <?php echo $height;?>;
-	// window.wp_redditjs.width=<?php echo $width;?>;
-	</script>
-		<?
+		echo "<script src='//localhost:8002/post.js' data-width='$width' data-height='$height' data-postSortOrder='$postSortOrder'></script>";
 
 	}
+
+
 
 
 	private function getBaseUrl(){
